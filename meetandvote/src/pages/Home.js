aedc24c03be7ea804891395aeb7154ff6cde6ae4
Onur Kafkas <onurkafkas2001@gmail.com>
2025-03-08 17:38:18 +0300

    Minor name change.

--- meetandvote/src/pages/Home.js
+++ meetandvote/src/pages/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
           className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition"
           onClick={() => navigate("/meeting")}
         >
-          Go to Meeting
+          Go to DiscussAndVote
         </button>
         <button
           className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition"
--- meetandvote/src/pages/Meeting.js
+++ meetandvote/src/pages/Meeting.js
@@ -1,7 +1,7 @@
 export default function Meeting() {
     return (
       <div className="flex items-center justify-center min-h-screen bg-gray-200">
-        <h1 className="text-2xl font-bold">Meeting Page</h1>
+        <h1 className="text-2xl font-bold">DiscussAndVote Page</h1>
       </div>
     );
   }
